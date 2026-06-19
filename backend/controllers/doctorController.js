fc978a2714e448bc75b04ffb933e4f8ca1ecc75f
Baibhav Sinha <baibhavsinha.dev@gmail.com>
2026-06-20 02:14:21 +0530

    CORS Configuration

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -44,7 +44,7 @@ export const doctorLogin = async (req, res) => {
         res.cookie("doctorToken", token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { About, Appointments, Auth, Contact, DoctorDetails, Doctors, Home, Profile } from "./pages/pages";
 import { Footer, Loading, Navbar, ScrollToTop } from "./components/components";
 import { ToastContainer } from "react-toastify"
+import { useAppContext } from './context/AppContext';
 
 const App = () => {
+
+    const { isAuth } = useAppContext();
+
     return (
         <div className="mx-4 sm:mx-[10%]">
             <ScrollToTop />
@@ -17,11 +21,11 @@ const App = () => {
                     <Route path='/doctors' element={<Doctors />} />
                     <Route path='/doctors/:speciality' element={<Doctors />} />
                     <Route path='/doctor/:docId' element={<DoctorDetails />} />
-                    <Route path='/auth' element={<Auth />} />
+                    <Route path='/auth' element={!isAuth ? <Auth /> : <Navigate to="/" replace />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/contact' element={<Contact />} />
-                    <Route path='/appointments' element={<Appointments />} />
-                    <Route path='/profile' element={<Profile />} />
+                    <Route path='/appointments' element={isAuth ? <Appointments /> : <Navigate to="/" replace />} />
+                    <Route path='/profile' element={isAuth ? <Profile /> : <Navigate to="/" replace />} />
                 </Routes>
             </Suspense>
 
